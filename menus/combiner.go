9a9daa6244bb96621dcdfb93c480d544329b1f5a
matthew <mattrspeiser@gmail.com>
2022-04-07 12:57:33 -0600

    updated cli options and collating

--- menus/combiner.go
+++ menus/combiner.go
@@ -5,23 +5,41 @@ import (
 )
 
 func collate(){
-  //allmenus := [][]string
+  allmenukeys := []string{
+  "main",
+  "main.state",
+  "main.pipeline",
+  "main.sources",
+  "main.destinations",
+  "main.transforms",
+  "main.sequences"}
+
+  menus := GetCliMap(allmenukeys)
+
   mainopts := []string{
-    "q quit main.quit",
-    "1 state main.state",
-    "2 pipelines main.pipeline",
-    "3 sources main.sources",
-    "4 destinations main.destinations",
-    "5 data transforms main.transforms",
-    "6 sequences main.sequences"}
-  MainMenu := Menu{}
-  MainMenuOpts := make([]MenuOption,len(mainopts))
+    "q quit main.quit exit",
+    "1 state main.state menu",
+    "2 pipelines main.pipeline menu",
+    "3 sources main.sources menu",
+    "4 destinations main.destinations menu",
+    "5 data transforms main.transforms menu",
+    "6 sequences main.sequences menu"}
+
+  combined := GetMenuMap("main",mainopts)
+
+  MainMenu := Menu{menuname:"main"}
+  MainMenuOpts := make([]MenuOption,len(combined["main"]))
 
   for i := 0; i < len(mainopts); i++ {
     stringOpt := mainopts[i]
     splitOpt := strings.Split(stringOpt, " ")
-    MainMenuOpts[i] = MenuOption{accessor:splitOpt[0],value:splitOpt[1],uniquemenukey:splitOpt[2]}
+    MainMenuOpts[i] = MenuOption{
+      accessor:splitOpt[0],
+      value:splitOpt[1],
+      uniquemenukey:splitOpt[2],
+      action:splitOpt[3]}
   }
   MainMenu.menuoptions = MainMenuOpts
-  PrintMenuOptions(MainMenu)
+  menus["main"] = MainMenu
+  PrintMenuOptions(menus["main"])
 }
--- menus/structures.go
+++ menus/structures.go
@@ -10,6 +10,7 @@ type MenuOption struct {
   uniquemenukey string
   accessor      string
   value         string
+  action        string
 }
 
 /*
@@ -23,11 +24,24 @@ type Menu struct {
 }
 
 /* 
+returns a map of the menu name and un-collated menu options
+   get the Menu by accessing the unique menu key
+*/
+func GetMenuMap(name string, menuopts []string) map[string][]string {
+  menumap := make(map[string][]string)
+  menumap[name] = menuopts
+  return menumap
+}
+
+/* 
 returns a map of all cli menu items
    get the Menu by accessing the unique menu key
 */
-func Getmap() map[string]Menu{
-  allmenus := make(map[string]Menu)
-  return allmenus
+func GetCliMap(keys []string) map[string]Menu{
+  cli := make(map[string]Menu)
+  for i:=0;i<len(keys);i++{
+    cli[keys[i]] = Menu{menuname:keys[i]}
+  }
+  return cli
 }
 
